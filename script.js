b4c278603f54fc882226f4c3afd9bc2dc4adcd00
Anders Greve Sørensen <a.grevesoerensen@gmail.com>
2023-01-07 10:13:49 +0100

    Add drawing mechanic to grid.
    Background color of each grid square now changes to black when left
    mousebutton is clicked and mouse enters a square.

--- script.js
+++ script.js
@@ -2,11 +2,36 @@ let paintingContainer = document.querySelector("#paintingContainer")
 
 function createDivGrid(n) {
     for (let i = 0; i < n * n; i++) {
-        let gridDiv = document.createElement("div")
-        gridDiv.setAttribute("id", "gridDiv")
-        paintingContainer.appendChild(gridDiv)
-        // console.log("adding a div")
+        let gridSquare = document.createElement("div")
+        gridSquare.setAttribute("id", "gridSquare")
+        paintingContainer.appendChild(gridSquare)
     }
 }
 
 createDivGrid(16)
+
+let drawState = false
+window.addEventListener("mousedown", (e) => {
+    if (e.button === 0) {
+        drawState = true
+    }
+})
+window.addEventListener("mouseup", (e) => {
+    if (e.button === 0) {
+        drawState = false
+    }
+})
+
+
+let gridSquares = document.querySelectorAll("#gridSquare")
+gridSquares.forEach((square) => square.addEventListener("mouseenter", (e) => {
+    if (drawState) {
+        square.classList.add("colored")
+    }
+}))
+
+
+
+// function hoverColorChange() {
+
+// }
